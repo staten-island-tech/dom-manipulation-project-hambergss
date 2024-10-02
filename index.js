4b02b6fb3acb5b0df6fe6a00644fb1b96735d45d
hambergss <144480774+hambergss@users.noreply.github.com>
2024-10-02 12:52:30 -0400

    s

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   items: document.querySelectorAll("li"),
   cardHeader: document.querySelector(".card-header"),
-  button: document.querySelector("btn"),
-  form: document.querySelector(".form"),
-};
-let student = {
-  name: "Ryan",
-  age: 15,
-  girlfriend: true,
+  forms: document.querySelectorAll(".form"),
 };
 
 DOMSelectors.button.addEventListener("click", function (event) {
   console.log(event.target.parentElement);
-  event.target.parentElement.style.backgroundColor = "red";
 });
-DOMSelectors.form.addEventListener("submit", function (event) {
+DOMSelectors.forms.addEventListener("submit", function (event) {
   event.preventDefault();
   document.querySelector("input").value;
 });
