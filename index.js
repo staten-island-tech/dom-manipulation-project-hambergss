02d36ff98f645aba33f8d99168605efcc3705825
hambergss <144480774+hambergss@users.noreply.github.com>
2024-10-09 13:57:50 -0400

    s

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   items: document.querySelectorAll("li"),
   cardHeader: document.querySelector(".card-header"),
-  forms: document.querySelectorAll(".form"),
+  button: document.querySelector("btn"),
+  form: document.querySelectorAll(".form"),
+  container: document.querySelector("container"),
 };
 
 DOMSelectors.button.addEventListener("click", function (event) {
   console.log(event.target.parentElement);
+  event.target.parentElement.style.backgroundColor = "red";
 });
-DOMSelectors.forms.addEventListener("submit", function (event) {
+DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  document.querySelector("input").value;
+  console.log(document.querySelector("input").value);
 });
+
+//select all buttons as nodelist (can use for each
+const buttons = document.querySelectorAll("button");
+//make array from buttons if i want to use filter etc.
+
+
+//create the HTML for inputs, cards, and container aka where the cards go
+
+//select/query the HTML form and get values from inputs
+
+//turn values into object Movie, Game, User, Whatevers
+
+//insert card with object onto HTML
+
+//add event listener for remove button in JS
